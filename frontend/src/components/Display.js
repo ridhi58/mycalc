9d32a20612c52056f226c18a2c998ea30697253a
ridhi58 <ridhiagarwal58@gmail.com>
2021-03-19 22:09:54 +0530

    UI updated

--- frontend/src/components/Display.js
+++ frontend/src/components/Display.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Display.css'
 
 export default function Display() {
@@ -26,42 +26,60 @@ export default function Display() {
             setResult(result + e.target.name)
         }
     }
+
+    const handleZero = (e) => {
+
+        if (result != "")
+            setResult(result + e.target.name)
+
+    }
+
+    useEffect(() => {
+
+        if (result.length == 13) {
+            setResult("limit reached")
+        }
+
+
+
+    }, [result])
     return (
-        <div>
-            <div>
+        <div className="display">
+            <div className="displayRes" >
                 {
                     on ? <span>{result}</span> : "off"
                 }
             </div>
             <div className="displayKeys">
+                <div>
+                    <button style={{ width: "90px" }} onClick={handleOnOff}>AC</button>
+                    <button onClick={() => { setResult("") }}>C</button>
+                </div>
                 <div >
                     <button name='7' onClick={handleButton}>7</button>
                     <button name='8' onClick={handleButton}>8</button>
                     <button name='9' onClick={handleButton}>9</button>
-                    <button name='/' onClick={handleButton}>/</button>
+                    <button style={{ backgroundColor: "orange" }} name='/' onClick={handleButton}>/</button>
                 </div>
-                <div>
+                <div className="displayKey">
                     <button name='4' onClick={handleButton}>4</button>
                     <button name='5' onClick={handleButton}>5</button>
                     <button name='6' onClick={handleButton}>6</button>
-                    <button name='*' onClick={handleButton}>*</button>
+                    <button style={{ backgroundColor: "orange" }} name='*' onClick={handleButton}>*</button>
                 </div>
                 <div>
                     <button name='1' onClick={handleButton}>1</button>
                     <button name='2' onClick={handleButton}>2</button>
                     <button name='3' onClick={handleButton}>3</button>
-                    <button name='-' onClick={handleButton}>-</button>
+                    <button style={{ backgroundColor: "orange" }} name='-' onClick={handleButton}>-</button>
                 </div>
                 <div>
-                    <button name='0' onClick={handleButton}>0</button>
+                    <button name='0' onClick={handleZero}>0</button>
                     <button name='.' onClick={handleDecimal}>.</button>
-                    <button name='=' onClick={handleResult}>=</button>
-                    <button name='+' onClick={handleButton}>+</button>
+                    <button style={{ backgroundColor: "orange" }} name='=' onClick={handleResult}>=</button>
+                    <button style={{ backgroundColor: "orange" }} name='+' onClick={handleButton}>+</button>
                 </div>
-                <div>
-                    <button onClick={handleOnOff}>AC</button>
-                    <button onClick={() => { setResult("") }}>C</button>
-                </div>
+
             </div>
         </div>
     );

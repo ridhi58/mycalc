a0e0604caa830dc219c9cc259b2ca0f83c29be11
ridhi58 <ridhiagarwal58@gmail.com>
2021-04-29 13:44:42 +0530

    persisting state of redux

--- frontend/src/redux/Store.js
+++ frontend/src/redux/Store.js
@@ -5,8 +5,32 @@ import { applyMiddleware } from 'redux'
 const reduxLogger = require('redux-logger')
 const logger = reduxLogger.createLogger()
 
+function saveToLocalStorage(state) {
+    try {
+        const myState = JSON.stringify(state)
+        localStorage.setItem('state', myState)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+function LoadFromLocalStorage() {
+    try {
+        const myState = localStorage.getItem('state')
+        if (myState == null)
+            return undefined
+        else
+            return JSON.parse(myState)
+    }
+    catch (e) {
+        console.log(e)
+        return undefined
+    }
+}
+
+const persistedState = LoadFromLocalStorage()
 
 
-const Store = createStore(LoginReducer, applyMiddleware(logger))
-
+const Store = createStore(LoginReducer, persistedState, applyMiddleware(logger))
+Store.subscribe(() => saveToLocalStorage(Store.getState()))
 export default Store
